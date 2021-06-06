0377a5b96c170c76dcb4117f60f72af4f56a3d76
vigneshwaran <78083959+vigneshsdev@users.noreply.github.com>
2021-06-06 09:56:22 +0530

    Update app-routing.module.ts

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -9,9 +9,9 @@ import {  StockDashboardComponent } from './components/stock-dashboard/stock-das
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'dashboard', component: StockDashboardComponent },
+  { path: 'dashboard', component: StockDashboardComponent,canActivate: [AuthGuardService] },
   { path: 'sign-up', component: RegisterComponent },
-  { path: 'simulations', component: SimulationComponent },
+  { path: 'simulations', component: SimulationComponent,canActivate: [AuthGuardService] },
   { path: 'companies', component: ManageCompanyComponent,canActivate: [AuthGuardService] },
 ];
 
